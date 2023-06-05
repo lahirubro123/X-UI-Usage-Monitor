58c2f9916420ec3bc27fb95d91cdef79ede456e3
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2023-06-05 20:18:29 +0530

    Update hosts.js

--- hosts.js
+++ hosts.js
@@ -13,6 +13,13 @@ hosts = {
         'path': '/etc/x-ui/x-ui.db',
         'table': 'client_traffics'
     },
+     'in': {
+        'ip': '0.0.0.0',
+        'user': 'root',
+        'password': 'root',
+        'path': '/etc/x-ui/x-ui.db',
+        'table': 'client_traffics'
+    },
 }
 
 module.exports = hosts;
